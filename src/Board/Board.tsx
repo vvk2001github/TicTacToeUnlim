ab76dc86ba84d708e07bc4ca369364c7f5e14a38
kulyaginv <kulyaginv@gmail.com>
2020-05-27 15:19:15 +0300

    20200527-1519

--- src/Board/Board.tsx
+++ src/Board/Board.tsx
@@ -2,30 +2,59 @@ import React from 'react'
 import './Board.css'
 import { Square } from '../Square'
 
-export class Board extends React.Component {
+export interface BoardState {
+    squares: Array<String>;
+    xIsNext: boolean;
+}
+
+export class Board extends React.Component<{}, BoardState> {
+    constructor(props: any) {
+        super(props);
+        this.state = {
+            squares: Array(9).fill(null),
+            xIsNext: true
+        };
+    }
+
+    public handleClick(i: number) {
+        const squares = this.state.squares.slice();
+        squares[i] = this.state.xIsNext ? 'X' : 'O';
+        this.setState({
+            squares: squares,
+            xIsNext: !this.state.xIsNext
+        });
+        console.log(squares)
+    }
+
+    public renderSquare(i: number) {
+        return (
+            <Square value={this.state.squares[i]} boardclick={() => this.handleClick(i)} />
+        );
+    }
+
     public render() {
         return (
             <div>
                 <div className="board-row">
-                    <Square value={''} num={1} boardclick = {this.click} />
-                    <Square value={''} num={2} boardclick = {this.click} />
-                    <Square value={''} num={3} boardclick = {this.click} />
+                    {this.renderSquare(0)}
+                    {this.renderSquare(1)}
+                    {this.renderSquare(2)}
                 </div>
                 <div className="board-row">
-                    <Square value={''} num={4} boardclick = {this.click} />
-                    <Square value={''} num={5} boardclick = {this.click} />
-                    <Square value={''} num={6} boardclick = {this.click} />
+                    {this.renderSquare(3)}
+                    {this.renderSquare(4)}
+                    {this.renderSquare(5)}
                 </div>
                 <div className="board-row">
-                    <Square value={''} num={7} boardclick = {this.click} />
-                    <Square value={''} num={8} boardclick = {this.click} />
-                    <Square value={''} num={9} boardclick = {this.click} />
+                    {this.renderSquare(6)}
+                    {this.renderSquare(7)}
+                    {this.renderSquare(8)}
                 </div>
         </div>
         )
     }
 
     public click(num: number) {
-        console.log('Click ' + String(num) + ' !!!')
+        console.log('Click ' + String(num) + ' !!!');
     }
 }
--- src/Square.tsx
+++ src/Square.tsx
@@ -2,22 +2,22 @@ import React from 'react'
 import './Square.css'
 
 export interface SquareProps {
-    value: string;
-    num: number;
-    boardclick: (num: number) => void;
+    boardclick: () => void;
+    value: String
 }
 
 export class Square extends React.Component<SquareProps, {}> {
+    constructor(props: SquareProps){
+        super(props)
+        this.state = {value: ''}
+    }
+
     public render() {
         return (
-            <button className="square" onClick={this.click}>
+            <button className="square" onClick={this.props.boardclick}>
                 {this.props.value}
             </button>
         )
-    }
-
-    public click = () => {
-        this.props.boardclick(this.props.num);
     }
 
 }
