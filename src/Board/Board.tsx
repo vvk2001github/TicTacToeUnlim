61a6cd226f08e15f87748ac1cc0d0e1579975114
kulyaginv <kulyaginv@gmail.com>
2020-05-27 15:56:29 +0300

    Winner

--- src/Board/Board.tsx
+++ src/Board/Board.tsx
@@ -18,6 +18,7 @@ export class Board extends React.Component<{}, BoardState> {
 
     public handleClick(i: number) {
         const squares = this.state.squares.slice();
+        if(squares[i] || (this.calculateWinner(squares) !== '')) return;
         squares[i] = this.state.xIsNext ? 'X' : 'O';
         this.setState({
             squares: squares,
@@ -33,8 +34,20 @@ export class Board extends React.Component<{}, BoardState> {
     }
 
     public render() {
+
+        const winner: String = this.calculateWinner(this.state.squares)
+        
+        let status: String;
+
+        if (winner !== '') {
+            status = 'Выиграл ' + String(winner)
+        } else {
+            status = 'Следующий ход: ' + (this.state.xIsNext ? 'X' : 'O');
+        }
+
         return (
             <div>
+                <div className="status">{status}</div>
                 <div className="board-row">
                     {this.renderSquare(0)}
                     {this.renderSquare(1)}
@@ -56,5 +69,25 @@ export class Board extends React.Component<{}, BoardState> {
 
     public click(num: number) {
         console.log('Click ' + String(num) + ' !!!');
+    }
+
+    private calculateWinner(squares: Array<String>): String {
+        const lines = [
+            [0, 1, 2],
+            [3, 4, 5],
+            [6, 7, 8],
+            [0, 3, 6],
+            [1, 4, 7],
+            [2, 5, 8],
+            [0, 4, 8],
+            [2, 4, 6],
+        ];
+        for (let i = 0; i < lines.length; i++) {
+            const [a, b, c] = lines[i];
+            if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+                return squares[a];
+            }
+        }
+        return '';
     }
 }
